52710328d2452505c7e7116e618cbee4c81801f1
MaoXianxin <My1044467857@yahoo.com>
2024-09-24 10:30:12 +0800

    hubserving

--- deploy_hubserving.sh
+++ deploy_hubserving.sh
@@ -9,9 +9,12 @@ hub install deploy/hubserving/ocr_rec_vis_gray
 hub install deploy/hubserving/ocr_rec_mrz
 
 # 启动服务
-nohup hub serving start -c deploy/hubserving/ocr_system/config.json > ocr_system_log.out 2>&1 &
-nohup hub serving start -c deploy/hubserving/ocr_rec_vis/config.json > vis_log.out 2>&1 &
-nohup hub serving start -c deploy/hubserving/ocr_rec_vis_gray/config.json > vis_gray_log.out 2>&1 &
-nohup hub serving start -c deploy/hubserving/ocr_rec_mrz/config.json > mrz_log.out 2>&1 &
+hub serving start -c deploy/hubserving/ocr_system/config.json > ocr_system_log.out 2>&1 &
+hub serving start -c deploy/hubserving/ocr_rec_vis/config.json > vis_log.out 2>&1 &
+hub serving start -c deploy/hubserving/ocr_rec_vis_gray/config.json > vis_gray_log.out 2>&1 &
+hub serving start -c deploy/hubserving/ocr_rec_mrz/config.json > mrz_log.out 2>&1 &
 
 echo "所有服务已启动"
+
+# 保持一个前台进程
+tail -f /dev/null
